b5e3df06e587906c700a3a40d4ce2148e0e5b518
estee994-hub <estee994@gmail.com>
2026-08-07 14:19:57 +0200

    Add files via upload

---
+++ card-data.js
@@ -0,0 +1,242 @@
+const CARD_DATA = [
+  {
+    "number": 1,
+    "unlocked": true,
+    "title": "Card 1",
+    "heading": "A little birthday surprise is waiting… 🌸",
+    "message": "The room named after something found in a spice rack holds today’s surprise. Head to the place where bubbles, shampoo and hot showers live… something beautiful is waiting there.",
+    "closing": "Happy Birthday! I wish I could celebrate this day with you."
+  },
+  {
+    "number": 2,
+    "unlocked": false,
+    "title": "Card 2",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 3,
+    "unlocked": false,
+    "title": "Card 3",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 4,
+    "unlocked": false,
+    "title": "Card 4",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 5,
+    "unlocked": false,
+    "title": "Card 5",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 6,
+    "unlocked": false,
+    "title": "Card 6",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 7,
+    "unlocked": false,
+    "title": "Card 7",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 8,
+    "unlocked": false,
+    "title": "Card 8",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 9,
+    "unlocked": false,
+    "title": "Card 9",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 10,
+    "unlocked": false,
+    "title": "Card 10",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 11,
+    "unlocked": false,
+    "title": "Card 11",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 12,
+    "unlocked": false,
+    "title": "Card 12",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 13,
+    "unlocked": false,
+    "title": "Card 13",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 14,
+    "unlocked": false,
+    "title": "Card 14",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 15,
+    "unlocked": false,
+    "title": "Card 15",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 16,
+    "unlocked": false,
+    "title": "Card 16",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 17,
+    "unlocked": false,
+    "title": "Card 17",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 18,
+    "unlocked": false,
+    "title": "Card 18",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 19,
+    "unlocked": false,
+    "title": "Card 19",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 20,
+    "unlocked": false,
+    "title": "Card 20",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 21,
+    "unlocked": false,
+    "title": "Card 21",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 22,
+    "unlocked": false,
+    "title": "Card 22",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 23,
+    "unlocked": false,
+    "title": "Card 23",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 24,
+    "unlocked": false,
+    "title": "Card 24",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 25,
+    "unlocked": false,
+    "title": "Card 25",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 26,
+    "unlocked": false,
+    "title": "Card 26",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 27,
+    "unlocked": false,
+    "title": "Card 27",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 28,
+    "unlocked": false,
+    "title": "Card 28",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 29,
+    "unlocked": false,
+    "title": "Card 29",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  },
+  {
+    "number": 30,
+    "unlocked": false,
+    "title": "Card 30",
+    "heading": "",
+    "message": "",
+    "closing": ""
+  }
+];
